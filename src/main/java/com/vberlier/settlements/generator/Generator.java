c55c001cf4c2c964bb7a8f0433565047a051fac1
Valentin Berlier <berlier.v@gmail.com>
2019-04-04 06:02:33 -0400

    Refactor height map and terrain map

--- src/main/java/com/vberlier/settlements/generator/Generator.java
+++ src/main/java/com/vberlier/settlements/generator/Generator.java
@@ -1,6 +1,9 @@
 package com.vberlier.settlements.generator;
 
+import net.minecraft.block.Block;
+import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
+import net.minecraft.util.EnumFacing;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
 import net.minecraft.world.gen.structure.StructureBoundingBox;
@@ -8,30 +11,63 @@ import net.minecraft.world.gen.structure.StructureBoundingBox;
 public class Generator {
     private final World world;
     private final StructureBoundingBox boundingBox;
-    private final HeightMap heightMap;
-    private final TerrainMap terrainMap;
+    private final int originX;
+    private final int originZ;
+    private final int sizeX;
+    private final int sizeZ;
+    private final BlockPos[][] heightMap;
+    private final BlockPos[][] terrainMap;
 
     public Generator(World world, StructureBoundingBox boundingBox) {
         this.world = world;
         this.boundingBox = boundingBox;
-        heightMap = new HeightMap(world, boundingBox);
-        heightMap.compute();
-        terrainMap = new TerrainMap(heightMap);
-        terrainMap.compute();
+
+        originX = boundingBox.minX;
+        originZ = boundingBox.minZ;
+        sizeX = boundingBox.getXSize();
+        sizeZ = boundingBox.getZSize();
+
+        heightMap = new BlockPos[sizeX][sizeZ];
+        terrainMap = new BlockPos[sizeX][sizeZ];
     }
 
     public void buildSettlement() {
-        for (BlockPos pos : heightMap.getPositions()) {
-            world.setBlockState(pos.add(0, 1, 0), Blocks.STAINED_GLASS.getDefaultState());
-        }
+        computeMaps();
 
-        for (BlockPos pos : terrainMap.getPositions()) {
-            world.setBlockState(pos, Blocks.STAINED_HARDENED_CLAY.getDefaultState());
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeZ; j++) {
+                world.setBlockState(heightMap[i][j].add(0, 1, 0), Blocks.STAINED_GLASS.getDefaultState());
+                world.setBlockState(terrainMap[i][j], Blocks.RED_NETHER_BRICK.getDefaultState());
+            }
         }
+    }
 
-        System.out.println("Height min y " + heightMap.getMinY());
-        System.out.println("Height max y " + heightMap.getMaxY());
-        System.out.println("Terrain min y " + terrainMap.getMinY());
-        System.out.println("Terrain max y " + terrainMap.getMaxY());
+    private void computeMaps() {
+        for (int i = 0; i < sizeX; i++) {
+            for (int j = 0; j < sizeZ; j++) {
+                int x = originX + i;
+                int z = originZ + j;
+
+                int y = world.getHeight(x, z);
+                BlockPos pos = new BlockPos(x, y, z);
+
+                heightMap[i][j] = pos;
+
+                while (nonTerrain(pos) && pos.getY() > 0) {
+                    pos = pos.down();
+                }
+
+                terrainMap[i][j] = pos;
+            }
+        }
+    }
+
+    private boolean nonTerrain(BlockPos pos) {
+        IBlockState state = world.getBlockState(pos);
+        Block block = state.getBlock();
+
+        return block.isAir(state, world, pos)
+                || block.isPassable(world, pos)
+                || block.isFlammable(world, pos, EnumFacing.UP);
     }
 }
