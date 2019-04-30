608b7236bbe3ba1a0863bce52782ae23fda91c2b
Valentin Berlier <berlier.v@gmail.com>
2019-04-30 11:01:20 -0400

    Version 0.1.1

--- src/main/java/com/vberlier/settlements/SettlementsMod.java
+++ src/main/java/com/vberlier/settlements/SettlementsMod.java
@@ -12,7 +12,7 @@ import org.apache.logging.log4j.Logger;
 public class SettlementsMod {
     public static final String MOD_ID = "settlements";
     public static final String NAME = "Settlements Mod";
-    public static final String VERSION = "0.1.0";
+    public static final String VERSION = "0.1.1";
 
     @Mod.Instance(MOD_ID)
     public static SettlementsMod instance;
